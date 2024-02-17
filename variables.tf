f6c4f0dc922f381762a0addf8297c7e8c1436bc5
suntsovvv <suntsovvv@gmail.com>
2024-02-17 14:23:51 +1000

    задача1

--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
 ###cloud vars
-variable "token" {
+/*variable "token" {
   type        = string
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
-
+*/
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default = "b1g6dgftb02k9esf1nmu"
 }
 
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default = ""
 }
 
 variable "default_zone" {
